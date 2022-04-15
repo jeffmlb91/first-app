0bd9bb64f6e0451215f10ffd4afaea0683bb01b3
jeffmlb91 <jeffmlb91@gmail.com>
2022-04-14 23:47:36 -0400

    update App.js - added function

--- src/App.js
+++ src/App.js
@@ -2,9 +2,20 @@
 import './App.css';
 
 function App() {
+
+  const name = "Jeff";
+  const isMale = true;
+
+  function getGreeting(user) {
+    if(user) {
+      return <h1>Hello, {user} </h1>
+    } return <h1>Hello, Stranger</h1>
+  }
   return (
     <div className="app">
-      <h1>Hello World</h1>
+      {getGreeting(name)}
+      <h1>Hello {name} </h1>
+      <h2>You are a {isMale ? 'Male' : 'Female'}</h2>
    
     </div>
   );
