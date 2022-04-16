829dfa6666c393d4a224f33a0aa2dc3f0b90eb65
jeffmlb91 <jeffmlb91@gmail.com>
2022-04-15 20:13:34 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
-
   const name = "Jeff";
   const isMale = true;
 
   function getGreeting(user) {
-    if(user) {
-      return <h1>Hello, {user} </h1>
-    } return <h1>Hello, Stranger</h1>
+    if (user) {
+      return <h1>Hello, {user} </h1>;
+    }
+    return <h1>Hello, Stranger</h1>;
   }
   return (
     <div className="app">
-      {getGreeting(name)}
-      <h1>Hello {name} </h1>
-      <h2>You are a {isMale ? 'Male' : 'Female'}</h2>
-   
+      {getGreeting()}
+      <h1 className="app__title">Hello {name} </h1>
+      <h2>You are a {isMale ? "Male" : "Female"}</h2>
+      <h1 className='app__title app__title--large'>This is a LARGE title</h1>
+      <h1 className='app__title app__title--small'>This is a SMALL title</h1>
     </div>
   );
 }
